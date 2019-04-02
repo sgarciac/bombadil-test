fe248813642bfec571b9bb60128832292d0cf35e
Sergio <sergio@demarque.com>
2019-04-02 11:59:59 -0400

    adapt test to new bombadil full typing encoder

--- main.ts
+++ main.ts
@@ -6,56 +6,59 @@ var reader = new toml.TomlReader();
 
 reader.readToml(input, true);
 
-function bombadilToTomlTestAtomicValue(input: toml.TomlValue) {
-  switch (input.type) {
-    case toml.atomicInteger: {
-      return { type: "integer", value: input.value.toString() };
-    }
-    case toml.atomicFloat: {
-      return { type: "float", value: input.value.toString() };
-    }
-    case toml.atomicNotANumber: {
-      return { type: "float", value: input.image.toString() };
-    }
-    case toml.atomicInfinity: {
-      return { type: "float", value: input.image.toString() };
-    }
-    case toml.atomicBoolean: {
-      return { type: "bool", value: input.image.toString() };
-    }
-    case toml.atomicString: {
-      return { type: "string", value: input.value };
-    }
-    case toml.localDateTime: {
-      return { type: "datetime-local", value: input.image };
-    }
-    case toml.offsetDateTime: {
-      return { type: "datetime", value: input.image };
-    }
-    case toml.localDate: {
-      return { type: "date", value: input.image };
-    }
-    case toml.localTime: {
-      return { type: "time", value: input.image };
-    }
-    default: {
-      throw `eh!? ${JSON.stringify(input)}`;
-    }
-  }
-}
+function bombadilToTomlTestAtomicValue(input: toml.TomlValue) {}
 
 function bombadilToTomlTest(input: any): any {
-  if (Object.prototype.hasOwnProperty.call(input, "type")) {
-    return bombadilToTomlTestAtomicValue(input);
-  } else if (input instanceof Array) {
+  if (input instanceof Array) {
     return input.map(bombadilToTomlTest);
   } else {
-    let newObj: { [key: string]: any } = {};
-    for (let property in input) {
-      newObj[property] = bombadilToTomlTest(input[property]);
+    switch (input.type) {
+      case toml.atomicInteger: {
+        return { type: "integer", value: input.value.toString() };
+      }
+      case toml.atomicFloat: {
+        return { type: "float", value: input.value.toString() };
+      }
+      case toml.atomicNotANumber: {
+        return { type: "float", value: input.image.toString() };
+      }
+      case toml.atomicInfinity: {
+        return { type: "float", value: input.image.toString() };
+      }
+      case toml.atomicBoolean: {
+        return { type: "bool", value: input.image.toString() };
+      }
+      case toml.atomicString: {
+        return { type: "string", value: input.value };
+      }
+      case toml.localDateTime: {
+        return { type: "datetime-local", value: input.image };
+      }
+      case toml.offsetDateTime: {
+        return { type: "datetime", value: input.image };
+      }
+      case toml.localDate: {
+        return { type: "date", value: input.image };
+      }
+      case toml.localTime: {
+        return { type: "time", value: input.image };
+      }
+      case toml.table: {
+        let newObj: { [key: string]: any } = {};
+        for (let property in input.content) {
+          newObj[property] = bombadilToTomlTest(input.content[property]);
+        }
+        return newObj;
+      }
+      default: {
+        throw `eh!? ${JSON.stringify(input)}`;
+      }
     }
-    return newObj;
   }
+  // if (Object.prototype.hasOwnProperty.call(input, "type")) {
+  //   return bombadilToTomlTestAtomicValue(input);
+  // } else  else {
+  // }
 }
 
 if (reader.errors.length > 0) {
